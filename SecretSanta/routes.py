fecda61a313ca39c7a031b86bbd40b426978c599
daniel-le18 <sonhailenguyen@gmail.com>
2021-01-01 01:44:24 -0600

    third day

--- SecretSanta/routes.py
+++ SecretSanta/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, flash, redirect, url_for, request
+from flask import render_template, flash, redirect, url_for, request, abort
 from SecretSanta import app, db, bcrypt
 from SecretSanta.forms import RegistrationForm, LoginForm, PostForm
 from SecretSanta.models import User, Post
@@ -19,8 +19,24 @@ def secretsanta():
 
 @app.route("/thanks")
 def thanks():
-    posts = Post.query.all()
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page,
+                                                                  per_page=12)
     return render_template("thanks.html", title="Thanks", posts=posts)
+
+
+@app.route("/user/<string:username>")
+def user_posts(username):
+    page = request.args.get('page', 1, type=int)
+    user = User.query.filter_by(username=username).first_or_404()
+
+    posts = Post.query.filter_by(author=user)\
+        .order_by(Post.date_posted.desc())\
+        .paginate(page=page,per_page=12)
+    return render_template("user_posts.html",
+                           title="User Post",
+                           posts=posts,
+                           user=user)
 
 
 @app.route("/about")
@@ -88,4 +104,48 @@ def new_post():
         db.session.commit()
         flash(f'Post has been created !')
         return redirect(url_for('thanks'))
-    return render_template("new_post.html", title="New Post", form=form)
+    return render_template("new_post.html",
+                           title="New Post",
+                           form=form,
+                           legend='New Post')
+
+
+@app.route("/thanks/<int:post_id>")
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('post.html', title=post.title, post=post)
+
+
+@app.route("/thanks/<int:post_id>/update", methods=['GET', 'POST'])
+@login_required
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    form = PostForm()
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash('Your post has been updated')
+        return redirect(url_for('post', post_id=post.id))
+    elif request.method == 'GET':
+        form.title.data = post.title
+        form.content.data = post.content
+
+    return render_template('new_post.html',
+                           title='Update Post',
+                           form=form,
+                           legend='Update Post')
+
+
+@app.route("/thanks/<int:post_id>/delete", methods=['GET', 'POST'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash(f'Post has been deleted !')
+    return redirect(url_for('thanks'))
