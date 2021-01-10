63ad201de3e524f1c0980097f5f084337a848e8d
daniel-le18 <sonhailenguyen@gmail.com>
2021-01-09 23:36:20 -0600

    done !

--- SecretSanta/main/utils.py
+++ SecretSanta/main/utils.py
@@ -4,21 +4,6 @@ import os
 
 
 def send_mail(email, name, message):
-    # myemail = os.environ.get('email')
-    # mypassword = os.environ.get('password')
-    # reciepient = os.environ.get('recipient')
-    # print(myemail, mypassword, reciepient)
-    # server = smtplib.SMTP("smtp.gmail.com", 587)
-    # try:
-    #     server.starttls()
-    #     server.login(myemail, mypassword)
-    #     message = name + "\n" + email + "\n" + message
-    #     server.sendmail(myemail, reciepient, message)
-    # except Exception as e:
-    #     print(e)
-    # finally:
-    #     server.quit()
-
     recipient = os.environ.get('recipient')
     msg = Message('Contact Me',
                   sender='no-reply@sneakyelf.com',
